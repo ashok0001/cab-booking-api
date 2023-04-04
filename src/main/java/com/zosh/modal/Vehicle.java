694059d5dbf73734eb183cf05aa261499780201a
Ashok Zarmariya <ashokzarmariya47@gmail.com>
2023-04-04 16:37:33 +0530

    create vehicle modal

--- src/main/java/com/zosh/modal/Vehicle.java
+++ src/main/java/com/zosh/modal/Vehicle.java
@@ -40,4 +40,114 @@ public class Vehicle {
     private boolean available;
 
     
+    public Vehicle() {
+		// TODO Auto-generated constructor stub
+	}
+
+    
+
+	public Vehicle(Long id, String make, String model, int year, String color, String licensePlate, int capacity,
+			boolean available) {
+		super();
+		this.id = id;
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.color = color;
+		this.licensePlate = licensePlate;
+		this.capacity = capacity;
+		this.available = available;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getMake() {
+		return make;
+	}
+
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+
+	public String getModel() {
+		return model;
+	}
+
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+
+	public int getYear() {
+		return year;
+	}
+
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+
+	public String getColor() {
+		return color;
+	}
+
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+
+
+	public void setLicensePlate(String licensePlate) {
+		this.licensePlate = licensePlate;
+	}
+
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Vehicle [id=" + id + ", make=" + make + ", model=" + model + ", year=" + year + ", color=" + color
+				+ ", licensePlate=" + licensePlate + ", capacity=" + capacity + ", available=" + available + "]";
+	}
+    
+    
+    
 }
