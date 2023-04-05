8c0d5bd368bb2b5ac7cdde743045085f3ed3ac2a
Ashok Zarmariya <ashokzarmariya47@gmail.com>
2023-04-05 12:49:09 +0530

    create auth controller

---
+++ src/main/java/com/zosh/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.zosh.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zosh.exception.UserException;
+import com.zosh.modal.User;
+import com.zosh.service.UserService;
+
+@RestController
+public class AuthController {
+
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/signup")
+	public ResponseEntity<User> findUserByIdHandler(User user)throws UserException{
+		
+		User createdUser = userService.createUser(user);
+		
+		return new ResponseEntity<User>(createdUser,HttpStatus.ACCEPTED);
+		
+	}
+}
