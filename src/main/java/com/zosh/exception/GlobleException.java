006645cd632d6b046a951ac2b34b4a4cc3d79211
Ashok Zarmariya <ashokzarmariya47@gmail.com>
2023-04-05 12:08:22 +0530

    handle exception

---
+++ src/main/java/com/zosh/exception/GlobleException.java
@@ -0,0 +1,37 @@
+package com.zosh.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobleException {
+	
+	@ExceptionHandler(UserException.class)
+	public ResponseEntity<ErrorDetails> userExceptionHandler(UserException ue, WebRequest req){
+		
+		ErrorDetails err=new ErrorDetails(ue.getMessage(),req.getDescription(false),LocalDateTime.now());
+		
+		
+		return new ResponseEntity<ErrorDetails>(err,HttpStatus.ACCEPTED);
+		
+	}
+	
+	
+	
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorDetails> otherExceptionHandler(UserException ue, WebRequest req){
+		
+		ErrorDetails err=new ErrorDetails(ue.getMessage(),req.getDescription(false),LocalDateTime.now());
+		
+		
+		return new ResponseEntity<ErrorDetails>(err,HttpStatus.ACCEPTED);
+		
+	}
+
+}
