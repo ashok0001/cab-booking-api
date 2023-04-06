2bd7237a072bb9ed4fa3b6859f3920442cef13fe
Ashok Zarmariya <ashokzarmariya47@gmail.com>
2023-04-06 10:21:06 +0530

    create driver repository

---
+++ src/main/java/com/zosh/repository/DriverRepository.java
@@ -0,0 +1,9 @@
+package com.zosh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zosh.modal.Driver;
+
+public interface DriverRepository extends JpaRepository<Driver, Integer> {
+
+}
