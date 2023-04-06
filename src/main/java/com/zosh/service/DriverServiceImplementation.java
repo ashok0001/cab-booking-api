08b0c5a0662baaad4e7bd8a8a2697f6bf5d8efea
Ashok Zarmariya <ashokzarmariya47@gmail.com>
2023-04-06 12:15:59 +0530

    annotete driver service impl class with service anotation

--- src/main/java/com/zosh/service/DriverServiceImplementation.java
+++ src/main/java/com/zosh/service/DriverServiceImplementation.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.zosh.modal.Driver;
 import com.zosh.repository.DriverRepository;
 
+@Service
 public class DriverServiceImplementation implements DriverService {
 	
 	@Autowired
